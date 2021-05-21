85d105a9f90eae1edc04be8fa97cab469c0a3ecd
Emily Hoult <emily@emilyhoult.com>
2021-05-21 12:47:52 +0200

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/lists', to: 'lists#index', as: :lists
+  root to: 'lists#index'
   post '/lists', to: 'lists#create'
   get '/lists/new', to: 'lists#new', as: :new_list
   get '/lists/:id', to: 'lists#show', as: :list
